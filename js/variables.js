f7bd0bbaf8ad8f9044a780d564d707c884c71f6a
Ethan <ethankremerdojc@gmail.com>
2026-03-16 11:01:52 -0600

    basic credits (need to become rawhtml)

--- js/variables.js
+++ js/variables.js
@@ -262,7 +262,12 @@ const PANES_CONTENT = {
       promptText: "outro",
       resultTag: "p",
       result: [
-        "Credits: ... At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga."
+        "CREDITS: ",
+        "Ascii Arts:",
+        "found at https://www.asciiart.eu/",
+        "Ascii Donut:", 
+        "found at https://surenenfiajyan.github.io/3d-ascii-donut-js/, though that was also a recreation of another."
+
       ]
     },
   ],
