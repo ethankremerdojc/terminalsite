0bb1ab6f187c848128bfed0ee21028aab114e2ee
Ethan <ethankremerdojc@gmail.com>
2026-03-25 14:40:17 -0600

    mobile

--- modules/terminals/toys/toys.js
+++ modules/terminals/toys/toys.js
@@ -51,10 +51,11 @@ const TOY_INFO_HTML = `
 <br />
 <b>FRACTAL</b>
 <p>Wrote this fractal generator from scratch, including the complex number library. This is based on the function: </p>
-<p>fc(z) = z^2 + c, and initial z is configurable, c is the point on the complex plane.</p>
+<h3>fc(z) = z^2 + c</h3>
+<p>Initial z is configurable, c is the point on the complex plane.</p>
 <b>SNAKE</b>
 <p>Snake can be controlled by pressing arrow keys, or by tapping the sides of the playfield with mouse or finger.</p>
-<p>The playfield is responsive and the width and height in 'cells' is dependent on the user's screen size. The downside that the game is harder the smaller the screen. The upside is... hmm... I'll come up with one later.</p>
+<p>The playfield is responsive and the width and height in 'cells' is dependent on the user's screen size. The downside that the game is harder the smaller the screen.</p>
 <b>HANGMAN</b>
 <p>One of the first programs I write in every new programming language I learn. I wrote it in rust once. Should be self explanatory. Stole a wordlist from some github post haha.</p>
 <b>DONUT</b>
